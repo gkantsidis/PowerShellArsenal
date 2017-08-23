86b3f39f028cf71af0a241ead275ea1431ad8653
Christos Gkantsidis <chrisgk@microsoft.com>
2017-08-23 16:10:18 +0100

    added another fork

--- SetupRepo.ps1
+++ SetupRepo.ps1
@@ -1,11 +1,17 @@
 #Requires -Version 3
 
 [CmdletBinding()]
-param(    
+param(
 )
 
 if ((git remote) -notcontains "official") {
     Write-Verbose -Message "Creating remote for official depot"
     git remote add --tags official https://github.com/mattifestation/PowerShellArsenal.git
     git fetch official
+}
+
+if ((git remote) -notcontains "lowleveldesign") {
+    Write-Verbose -Message "Creating remote for lowleveldesign fork"
+    git remote add --tags lowleveldesign https://github.com/lowleveldesign/PowerShellArsenal.git
+    git fetch lowleveldesign
 }
